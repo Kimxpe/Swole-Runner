5c79659de387ea06837693c9bd077ad796b31709
Kim <Kim@Kimberlys-MBP.wireless.newschool.edu>
2017-03-29 16:48:43 -0400

    Some changes

---
+++ Assets/RestartAfterFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartAfterFall : MonoBehaviour {
+
+	private Vector3 MovePos;
+
+	void Update () {
+		
+		MovePos.y = 0;
+	}
+
+	void OnTriggerEnter (Collider other) {
+
+		if (other.CompareTag("Player")) {
+			SceneManager.LoadScene ("Main 1"); //change to load GAME OVER STAY 
+											   //SMALL Menu
+
+		}	
+	}
+}
